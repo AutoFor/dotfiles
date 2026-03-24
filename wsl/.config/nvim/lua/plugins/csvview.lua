22102a72a508c261335f177b058ffec19e8b1d59
your-name <your-id+your-name@users.noreply.github.com>
2026-03-25 04:34:03 +0900

    chore: update files

--- wsl/.config/nvim/lua/plugins/csvview.lua
+++ wsl/.config/nvim/lua/plugins/csvview.lua
@@ -2,4 +2,14 @@ return {
   "hat0uma/csvview.nvim",
   opts = {},
   cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+  ft = { "csv", "tsv" },
+  config = function()
+    require("csvview").setup()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "csv", "tsv" },
+      callback = function()
+        require("csvview").enable()
+      end,
+    })
+  end,
 }
