00968f0690321d967e3d55ef82193f1d5f07c305
your-name <your-id+your-name@users.noreply.github.com>
2026-03-30 05:53:47 +0900

    chore: update files

--- wsl/.config/nvim/lua/plugins/csvview.lua
+++ wsl/.config/nvim/lua/plugins/csvview.lua
@@ -1,14 +1,25 @@
 return {
   "hat0uma/csvview.nvim",
-  opts = {},
+  opts = {
+    view = {
+      display_mode = "border",
+      header_lnum = true,
+      sticky_header = { enabled = true },
+    },
+  },
   cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
   ft = { "csv", "tsv" },
-  config = function()
-    require("csvview").setup()
+  config = function(_, opts)
+    require("csvview").setup(opts)
     vim.api.nvim_create_autocmd("FileType", {
       pattern = { "csv", "tsv" },
       callback = function()
         require("csvview").enable()
+        -- Tab/S-Tab でファイル全体のコンマを前後に移動（検索レジスタを汚さない）
+        vim.keymap.set("n", "<Tab>", function() vim.fn.search(",") end,
+          { buffer = true, silent = true, desc = "Next comma" })
+        vim.keymap.set("n", "<S-Tab>", function() vim.fn.search(",", "b") end,
+          { buffer = true, silent = true, desc = "Previous comma" })
       end,
     })
   end,
